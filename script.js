a2b52662edb79b360aab4c4a8517ef5f4064a71b
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-02-27 18:34:36 +0530

    change direction done

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const unitSize = 25;
 
 let running = false;
 let xVelocity = unitSize;
-let yVelocity = unitSize;
+let yVelocity = 0;
 let xFood;
 let yFood;
 let score = 0;
@@ -23,6 +23,8 @@ let snake = [
     {x: unitSize, y:0},
     {x: 0, y:0}
 ]
+
+window.addEventListener("keydown", changeDirection);
 
 gameStart();
 // reset();
@@ -95,4 +97,35 @@ function drawSnake(){
         ctx.strokeRect(snakePart.x, snakePart.y, unitSize, unitSize);
     })
 };
-function changeDirection(){};
+function changeDirection(event){
+    const keyPressed = event.keyCode;
+    const UP = 38;
+    const DOWN = 40;
+    const LEFT = 37;
+    const RIGHT = 39;
+
+    const goingUp = (yVelocity == -unitSize);
+    const goingDown = (yVelocity == unitSize);
+    const goingRight = (xVelocity == unitSize);
+    const goingLeft = (xVelocity == -unitSize);
+
+    switch(true){
+        case(keyPressed == LEFT && !goingRight):
+            xVelocity = -unitSize;
+            yVelocity = 0;
+            break;
+        case(keyPressed == UP && !goingDown):
+            xVelocity = 0;
+            yVelocity = -unitSize;
+            break;
+        case(keyPressed == RIGHT && !goingLeft):
+            xVelocity = unitSize;
+            yVelocity = 0;
+            break;
+        case (keyPressed == DOWN && !goingUp):
+            xVelocity = 0;
+            yVelocity = unitSize;
+            break;
+            
+    }
+};
